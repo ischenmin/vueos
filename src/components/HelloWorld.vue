5842c99e3e06cbc5c7c8003fc795a4a4a5444285
ischenmin <coolskychenmin@yeah.net>
2018-01-25 17:32:46 +0800

    项目中使用vw实现移动端适配

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -81,6 +81,11 @@
       </li>
     </ul>
   </div>
+<!--  <ul id="nav">
+    <li v-for="item in items">
+      {{ item.message }}
+    </li>
+  </ul>-->
 </template>
 
 <script>
@@ -109,5 +114,37 @@ li {
 }
 a {
   color: #42b983;
+}
+#nav {
+  position:relative;
+  box-sizing: border-box;
+  min-height: 1rem;
+  margin-bottom:0.22rem;
+  width:100%;
+  height: 9.08rem;
+  display: -webkit-flex; /* Safari */
+  display: flex;
+  flex-flow: row wrap;
+  align-content:space-between;
+  margin-top:0.5rem;
+  padding:0 3%;
+}
+#nav li {
+  display:black;
+  box-sizing: border-box;
+  height: 2.77rem;
+  flex: 1 0 20%;
+  background:url();
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  border-radius:0.2rem;
+}
+#nav li:nth-child(odd){margin-right:3%;}
+#nav li.active {
+  -webkit-box-shadow:0 0 5px 3px #fa2f50;
+  -moz-box-shadow:0 0 5px 3px #fa2f50;
+  box-shadow:0 0 5px 3px #fa2f50;
+
 }
 </style>
